93e69e2eb461fa09236b85dde6de9b16ce0027bd
Kidplayer666 <67331281+Kidplayer666@users.noreply.github.com>
2020-06-24 13:03:54 +0100

    Update game.py
    A score system

--- game.py
+++ game.py
@@ -1,10 +1,8 @@
 from random import randint, shuffle
-from guizero import Waffle, App, PushButton, Window, Text
+from guizero import Waffle, App, PushButton, Text
 
 """This is supposed to be a simple minigame where you try to protect against fires"""
-def startgame():
-    startmenu.hide()
-    app.show()
+
 
 # picks a box's adjacent box (includes diagonals): the x and y differences are less than 2, at least one of them is 1
 def adjacent(pixel):                                                                           # and if it is neutral
@@ -19,6 +17,7 @@ def adjacent(pixel):                                                            
             return second_pixel
     return pixel      # else it returns the original pixel
 
+
 # dictates the fire spread: picks a random cell adjacent to a fire cell and makes it a fire cell, for all fire cells
 def propagate():
     spreadables = []
@@ -27,18 +26,20 @@ def propagate():
             spreadables.append(adjacent(source))
     for pixel in spreadables:
         pixel.color = "red"
+    scorer()          # updates the scoreboard
 
 
 """This ignites the fire"""
 
 
 def lighter():
-    x1 = randint(0, 10)
-    y1 = randint(0, 10)
+    x1 = randint(0, waffle.width - 1)
+    y1 = randint(0, waffle.height - 1)
     waffle.set_pixel(x1, y1, "red")
-    x2 = randint(0, 10)
-    y2 = randint(0, 10)
+    x2 = randint(0, waffle.width - 1)
+    y2 = randint(0, waffle.height - 1)
     waffle.set_pixel(x2, y2, "red")
+    scorer()    # updates the scoreboard
 
 
 """this resets the whole dam thing"""
@@ -56,21 +57,14 @@ def protec(a, b):
     thepixelcolor = waffle.get_pixel(a, b)
     if thepixelcolor != "red":
         waffle.set_pixel(a, b, "blue")
+    scorer()       # updates the scoreboard
 
 
 """basic GUI setup"""
-app = App(layout="grid", height=625, width=500)
-waffle = Waffle(app, pad=0, grid=[1, 1], height=20, width=20, color="green", command=protec, dim=25)
-restbutton = PushButton(app, text="reset", command=resetthething, grid=[1, 2])
-propagatebutton = PushButton(app, text="propagate", command=propagate, grid=[1, 3])
-"""Start menu stuff"""
-startmenu=Window(app, title="Welcome", height=200, width=550)
-introtext=Text(startmenu, text="Welcome to this tiny random game")
-Instructions=Text(startmenu, size=9, text="Instructions: The objective is to protect as much of the forest(green) ")
-Instructions2=Text(startmenu, size=9, text=" while using the least water (left mouse click that turns healthy forest into protected blue areas)")
-Instructions3=Text(startmenu, size=9, text="from the fire(red)")
-startbutton=PushButton(startmenu, text="Start", command=startgame)
-credits=Text(startmenu, text="Made by The Cool Guy 468 and Kidplayer_666", size=8)
+app = App(layout="grid", height=700, width=1000)
+waffle = Waffle(app, pad=0, grid=[1, 1], height=10, width=10, color="green", command=protec, dim=50)
+reset_button = PushButton(app, text="reset", command=resetthething, grid=[1, 2])
+propagate_button = PushButton(app, text="propagate", command=propagate, grid=[1, 3])
 
 pixels = []
 for x in range(waffle.width):
@@ -78,11 +72,25 @@ for x in range(waffle.width):
         pixels.append(waffle.pixel(x, y))
 shuffle(pixels)
 
+scoreboard = Text(app, f"Score = {waffle.height * waffle.width}", 20, "black", grid=[waffle.height, waffle.width])
+
+
+def scorer():                     # score is the number of neutral cells
+    score = waffle.width * waffle.height
+    for pixel in pixels:
+        if pixel.color == "blue":
+            score -= 1
+        if pixel.color == "red":
+            score -= 1
+    scoreboard.value = f"Score = {score}"
+
+
 """just starting the app"""
 lighter()
 
-app.repeat(750, propagate)
-startmenu.show()
-app.hide()
-app.display()
+app.repeat(1000, propagate)
 
+
+"""above theres quite a high time before propagating due to error in function. Now it doesnt exist, theres a button 
+to do it manually, this comment is to signal where to put this """
+app.display()
